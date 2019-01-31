8e08663586ccab897cd6ea6ad705720bb48cf3ce
Cai Xingliang <xingliang.cai@nio.com>
2019-01-31 11:30:24 +0800

    Add comments for arguments!

--- src/binarySearcher.js
+++ src/binarySearcher.js
@@ -1,20 +1,13 @@
 
 function findIndex( sortedArr ,findKey){
-   
+   // sortedArr: sorted array of integer in ascending order
+   // findKey: integer to be found in the sortedArray
     return -1
 }
 
 function binarySearchIndex(arr,findKey,start,end ){
     
-    if (start > end ) return -1;
-    var mid= Math.floor((start + end) / 2)
-    if (findKey ==arr[mid]){
-        return mid;
-    }else if (findKey < arr[mid]){
-        return binarySearch(arr, findKey, start, mid-1)
-    }else{
-        return binarySearch(arr, findKey, mid+1, end)
-    }
+
 
 }
 
