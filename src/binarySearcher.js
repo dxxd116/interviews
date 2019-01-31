93d964f02f6f4f161db527d76c3b4ba2b4690b0b
Cai Xingliang <xingliang.cai@nio.com>
2019-01-31 14:15:04 +0800

    Tets

--- src/binarySearcher.js
+++ src/binarySearcher.js
@@ -3,23 +3,6 @@ function findIndex( sortedArr ,findKey){
    // sortedArr: sorted array of integer in ascending order
    // findKey: integer to be found in the sortedArray
     
-   var low=0
-   var high=sortedArr.length-1
-
-   while (low <= high){
-   var mid=Math.floor((high+low) / 2)
-
-   if (findKey==sortedArr[mid]){
-       return mid
-   }
-   else if(findKey < sortedArr[mid]){
-       high=mid-1
-   }else {
-       low=mid+1
-   }
-   }
-   return -1
-
 }
 
 function binarySearchIndex(sortedArr,findKey,start,end ){
